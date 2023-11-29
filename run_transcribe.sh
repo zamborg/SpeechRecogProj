ba4058335e5234bf5639b8c85bd83f5b817e06ba
Zubin Aysola <zubinaysola@gmail.com>
2023-11-29 13:28:58 -0500

    f

--- run_transcribe.sh
+++ run_transcribe.sh
@@ -5,7 +5,7 @@
 #SBATCH --partition=GPU-shared
 #SBATCH --gres=gpu:v100-32:1
 #SBATCH --time=24:00:00
-#SBATCH --mem=10GB
+#SBATCH --mem=32GB
 #SBATCH --nodes=1
 # #SBATCH --ntasks-per-node=4
 #SBATCH --cpus-per-task=5
