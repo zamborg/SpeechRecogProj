e506b6021c25dec41cce7d89bd5cd88e645c31c5
Zubin Aysola <zubinaysola@gmail.com>
2023-11-28 22:33:04 -0500

    emerg

--- run_download.sh
+++ run_download.sh
@@ -5,7 +5,9 @@
 #SBATCH --partition=RM-shared
 #SBATCH --time=24:00:00
 #SBATCH --mem=32GB
-# #SBATCH --cpus-per-task=16
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=16
 
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate project
