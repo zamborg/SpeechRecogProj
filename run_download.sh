c870f2a8df6f446cf26592e85ffe4519524d0604
Zubin Aysola <zubinaysola@gmail.com>
2023-11-28 22:33:35 -0500

    f

--- run_download.sh
+++ run_download.sh
@@ -4,10 +4,10 @@
 #SBATCH --output=%x.out
 #SBATCH --partition=RM-shared
 #SBATCH --time=24:00:00
-#SBATCH --mem=32GB
+#SBATCH --mem=16GB
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=12
 
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate project
