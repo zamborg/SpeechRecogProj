8089e43898bd4e929a7bbea727a0078e91cd8bbb
Zubin Aysola <zubinaysola@gmail.com>
2023-11-28 23:06:44 -0500

    push

---
+++ download_mt.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# pip install -r req.txt
+# mkdir audio
+# git clone https://github.com/facebookresearch/voxpopuli.git
+cd voxpopuli
+
+# download data:
+ROOT='../../data'
+GREEK='el'
+LATVIAN='lv'
+MALTESE='mt'
+
+# python -m voxpopuli.download_audios --root $ROOT --subset $GREEK
+# python -m voxpopuli.download_audios --root $ROOT --subset $LATVIAN
+python -m voxpopuli.download_audios --root $ROOT --subset $MALTESE
+
+# now segment the data:
+# python -m voxpopuli.get_unlabelled_data --root $ROOT --subset $GREEK
+# python -m voxpopuli.get_unlabelled_data --root $ROOT --subset $LATVIAN
+python -m voxpopuli.get_unlabelled_data --root $ROOT --subset $MALTESE
+
+cd .. # move back dir
