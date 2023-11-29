22a382c01f82ff9daf11b04decfb3e38d04755ea
Zubin Aysola <zubinaysola@gmail.com>
2023-11-28 22:47:19 -0500

    shifting para

--- run_download.sh
+++ run_download.sh
@@ -6,8 +6,8 @@
 #SBATCH --time=24:00:00
 #SBATCH --mem=16GB
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=12
+#SBATCH --ntasks-per-node=12 # 12 tasks with 1 cpu each meaning 12 workers with 1cpu
+#SBATCH --cpus-per-task=1
 
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate project
