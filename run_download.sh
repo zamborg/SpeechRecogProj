adc85a21fdf0a916a75a8e3f05590f7a64668915
Ubuntu <ubuntu@ip-172-31-65-21.ec2.internal>
2023-11-29 02:21:54 +0000

    commit

--- run_download.sh
+++ run_download.sh
@@ -4,7 +4,7 @@
 #SBATCH --output=%x_%j.out
 #SBATCH --partition=RM-shared
 #SBATCH --time=24:00:00
-#SBATCH --cpus-per-task=32
+#SBATCH --cpus-per-task=16
 
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate project
